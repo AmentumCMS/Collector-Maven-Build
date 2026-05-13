fff03df272fff5c69ef5f094ce9bfa244f1287dd
KronfeldAmentum <william.kronfeld@us.amentum.com>
2026-05-13 13:20:36 -0600

    Add ActiveProfiles annotation for test configuration

--- src/test/java/com/jacobsfederal/demo/DemoApplicationTests.java
+++ src/test/java/com/jacobsfederal/demo/DemoApplicationTests.java
@@ -3,9 +3,11 @@ package com.amentumcms.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @Import(TestDemoApplication.class)
+@ActiveProfiles("test")
 class DemoApplicationTests {
 
 	@Test
